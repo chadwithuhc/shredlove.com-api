115319c017c6abcad46dd66e377f4acbc4cff769
chadwithuhc <chadwithuhc@gmail.com>
2020-07-03 00:43:44 -0600

    Disable writing in prod

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,14 +1,20 @@
 // Application hooks that run for every service
 
+function disabledProdRequests(context) {
+  if (!context.app.get('allowUpdates')) {
+    throw new Error('Access Denied')
+  }
+}
+
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [ disabledProdRequests ],
+    update: [ disabledProdRequests ],
+    patch: [ disabledProdRequests ],
+    remove: [ disabledProdRequests ]
   },
 
   after: {
